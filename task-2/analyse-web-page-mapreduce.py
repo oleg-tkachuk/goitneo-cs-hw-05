ddaf2fcf689472a7d8db7f70d3c00a860cfe6cf6
Oleg Tkachuk <oleg-tkachuk@users.noreply.github.com>
2024-03-20 13:45:11 +0200

    refactor(analyse-web-page-mapreduce.py): use threading processing instead of multiprocessing

--- task-2/analyse-web-page-mapreduce.py
+++ task-2/analyse-web-page-mapreduce.py
@@ -4,41 +4,42 @@ try:
     import sys
     import requests
     import argparse as ap
-    import collections as c
-    import multiprocessing as mp
+    import threading as th
+    import collections as col
     import matplotlib.pyplot as plt
 except ModuleNotFoundError as e:
     print(f"Error: {e}")
     exit(1)
 
-
 def get_url(url):
     return requests.get(url).text
 
-
-def map_function(text):
+def map_function(text, result_list):
     words = re.findall(r'\w+', text.lower())
-    return c.Counter(words)
-
+    result_list.append(col.Counter(words))
 
 def reduce_function(mapping_list):
-    return sum(mapping_list, c.Counter())
-
+    return sum(mapping_list, col.Counter())
 
 def chunkify(text, num_chunks):
     chunk_size = len(text) // num_chunks
     return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
 
+def map_reduce(text, num_threads=4):
+    chunks = chunkify(text, num_threads)
+    result_list = []
 
-def map_reduce(text, num_processes=4):
-    chunks = chunkify(text, num_processes)
+    threads = []
+    for chunk in chunks:
+        t = th.Thread(target=map_function, args=(chunk, result_list))
+        threads.append(t)
+        t.start()
 
-    with mp.Pool(num_processes) as pool:
-        mapped = pool.map(map_function, chunks)
-        reduced = reduce_function(mapped)
+    for thread in threads:
+        thread.join()
 
+    reduced = reduce_function(result_list)
     return reduced
-
 
 def visualize_top_words(word_counts, top=10):
     top_words = word_counts.most_common(top)
@@ -51,7 +52,6 @@ def visualize_top_words(word_counts, top=10):
     plt.xticks(rotation=45)
     plt.show()
 
-
 def cli():
     parser = ap.ArgumentParser(
         description='Analysing a web page using the MapReduce model')
@@ -59,18 +59,16 @@ def cli():
                         help='Address of the http web page for analysis')
     parser.add_argument('--words', '-w', required=False, type=int, default=10,
                         help='Number of top words to analyse (default: %(default)s)')
-    parser.add_argument('--processes', '-p', required=False, type=int,
-                        default=5, help='Number of processes to use (default: %(default)s)')
+    parser.add_argument('--threads', '-t', required=False, type=int,
+                        default=5, help='Number of threads to use (default: %(default)s)')
 
     return parser.parse_args()
-
 
 def main():
     args = cli()
     text = get_url(args.url)
-    word_counts = map_reduce(text, args.processes)
+    word_counts = map_reduce(text, args.threads)
     visualize_top_words(word_counts, args.words)
-
 
 if __name__ == "__main__":
     try:
